cb7318577b1452156fb284688b60294c5a1c6c04
nikita <nikitakapyckij@gmail.com>
2026-05-12 11:15:06 +0300

    день 20

---
+++ Day20/MiniBlog/MiniBlog/Controllers/PostsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniBlog.Models;
+
+namespace MiniBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: /Posts/Index
+        // Список всех постов
+        public IActionResult Index()
+        {
+            var posts = PostRepository.GetAll();
+            return View(posts);
+        }
+
+        // GET: /Posts/Details/1
+        // Маршрут: /Posts/Details/{id}
+        public IActionResult Details(int id)
+        {
+            var post = PostRepository.GetById(id);
+
+            if (post == null)
+            {
+                return NotFound($"Пост с Id={id} не найден");
+            }
+
+            return View(post);
+        }
+
+        // GET: /Posts/Create
+        // Показать форму создания
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Posts/Create
+        // Обработать данные из формы
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                PostRepository.Add(post);
+                // После создания перенаправляем на список
+                return RedirectToAction("Index");
+            }
+
+            // Если ошибки - показываем форму снова
+            return View(post);
+        }
+    }
+}
---
+++ Day20/MiniBlog/MiniBlog/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите заголовок")]
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Введите содержимое")]
+        [Display(Name = "Содержимое")]
+        public string Content { get; set; }
+
+        [Display(Name = "Дата публикации")]
+        public DateTime DatePosted { get; set; }
+    }
+}
